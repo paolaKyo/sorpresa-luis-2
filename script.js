944f3c7d217c3225b9b70a2b086d6fff70a5ea3f
paolaKyo <paolacalderonjip@gmail.com>
2025-09-30 11:19:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const modalImg = document.getElementById("modal-img");
 const modalFrase = document.getElementById("modal-frase");
 const cerrar = document.getElementById("cerrar");
 const final = document.getElementById("final");
-const corazones = document.getElementById("corazones");
 const musica = document.getElementById("musica");
 
 let contador = 0;
@@ -15,7 +14,7 @@ const flores = document.querySelectorAll(".flores img");
 btnIniciar.addEventListener("click", () => {
   portada.classList.add("oculto");
   galeria.classList.remove("oculto");
-  musica.play().catch(()=>{}); // música al iniciar
+  musica.play();
 });
 
 flores.forEach((flor, index) => {
@@ -24,12 +23,10 @@ flores.forEach((flor, index) => {
     modalFrase.textContent = flor.getAttribute("data-frase");
     modal.classList.remove("oculto");
     contador++;
-
     if (contador === flores.length) {
       setTimeout(() => {
         galeria.classList.add("oculto");
         final.classList.remove("oculto");
-        lanzarCorazones();
       }, 2000);
     }
   });
@@ -38,17 +35,3 @@ flores.forEach((flor, index) => {
 cerrar.addEventListener("click", () => {
   modal.classList.add("oculto");
 });
-
-function lanzarCorazones() {
-  for (let i = 0; i < 30; i++) {
-    let heart = document.createElement("span");
-    heart.textContent = "❤️";
-    heart.style.left = Math.random() * 100 + "vw";
-    heart.style.animationDuration = 4 + Math.random() * 4 + "s";
-    corazones.appendChild(heart);
-
-    setTimeout(() => {
-      heart.remove();
-    }, 8000);
-  }
-}
